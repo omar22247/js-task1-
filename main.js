dac199a1164b97ea9b47ca261508a657fb9e9dc2
omar <ohashim530@gmail.com>
2024-07-20 14:46:58 +0300

    done

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-// ---------------even or odd----------------------
-document.write(`<p style="background-color: blue; font-size:xx-large; margin-left: auto; margin-right: auto; width: fit-content;">---------------even or odd----------------------</p>
-    `);
-let a = -4;
-if (a == 0) {
-  document.write(`<p style="color: blue; font-size: xx-large;">Zero</p>`);
-} else {
-  if (a % 2 == 0) {
-    document.write(
-      `<p style="color: blue; font-size: xx-large;">(${a}) is even</p>`
-    );
-  } else {
-    document.write(
-      `<p style="color: blue; font-size: xx-large;">(${a}) is odd</p>`
-    );
-  }
-}
-// ------------------------Circumference and CircleAria ------------------------------
+// // ---------------even or odd----------------------
 
-document.write(`<p style="background-color: blue; font-size:xx-large; margin-left: auto; margin-right: auto; width: fit-content;">------------Circumference and CircleAria-----------------</p>
-    `);
-let radius = 10;
-let Circumference = radius * 2 * Math.PI;
-let CircleAria = Math.pow(radius, 2) * Math.PI;
-document.write(`<p style="color: blue; font-size: xx-large;">Circumference = ${Circumference}</p>
-    <p style="color: red; font-size: xx-large;">Circle Aria = ${CircleAria}</p>`);
+
+//     let a = parseFloat(prompt("Enter a number to check if it's even or odd:"));
+//     document.write(a);
+
+//     if (a == 0) {
+//         document.write(`<p style="color: blue; font-size: xx-large;">Zero</p>`);
+//     } else {
+//         if (a % 2 == 0) {
+//             document.write(
+//                 `<p style="color: blue; font-size: xx-large;">(${a}) is even</p>`
+//             );
+//         } else {
+//             document.write(
+//                 `<p style="color: blue; font-size: xx-large;">(${a}) is odd</p>`
+//             );
+//         }
+//     }
+
+    // ------------------------Circumference and CircleAria ------------------------------
+
+
+// let radius = parseFloat(prompt("Enter the radius of the circle:"));;
+// let Circumference = radius * 2 * Math.PI;
+// let CircleAria = Math.pow(radius, 2) * Math.PI;
+// document.write(`<p style="color: blue; font-size: xx-large;">Circumference = ${Circumference}</p>
+//     <p style="color: red; font-size: xx-large;">Circle Aria = ${CircleAria}</p>`);
 
 // -----------------------  + or -   -------------------------
 
-document.write(`<p style="background-color: blue; font-size:xx-large; margin-left: auto; margin-right: auto; width: fit-content;">------------ + or - -----------------</p>
-    `);
-let num1 = 10,
-  num2 = -8;
+let num1 = parseFloat(prompt("Enter the first number:"));
+  let num2 = parseFloat(prompt("Enter the second number:"));
 if (num1 == 0) {
   document.write(
     `<span style="color: black; font-size: xx-large;">Zero and </span>`
@@ -59,3 +59,4 @@ if (num2 == 0) {
     );
   }
 }
+
