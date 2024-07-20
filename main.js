60dc6f78cf43dc7b46599cf9f6b8970a28e8389a
omar <ohashim530@gmail.com>
2024-07-20 15:23:25 +0300

    done

--- main.js
+++ main.js
@@ -1,69 +1,57 @@
 // // ---------------even or odd----------------------
 
 
-//     let a = prompt("Enter a number to check if it's even or odd:");
-//     document.write(a);
-
-//     if (a == 0) {
+// let num = Number(prompt("Enter a number to check if it's even or odd:"));
+// if (num == 0) {
 //         document.write(`<p style="color: blue; font-size: xx-large;">Zero</p>`);
 //     } else {
-//         if (a % 2 == 0) {
+//         if (num % 2 == 0) {
 //             document.write(
-//                 `<p style="color: blue; font-size: xx-large;">(${a}) is even</p>`
+//                 `<p style="color: blue; font-size: xx-large;">(${num}) is even</p>`
 //             );
 //         } else {
 //             document.write(
-//                 `<p style="color: blue; font-size: xx-large;">(${a}) is odd</p>`
+//                 `<p style="color: blue; font-size: xx-large;">(${num}) is odd</p>`
 //             );
 //         }
 //     }
 
+
     // ------------------------Circumference and CircleAria ------------------------------
-
-
-// let radius = prompt("Enter the radius of the circle:");;
-// let Circumference = radius * 2 * Math.PI;
-// let CircleAria = Math.pow(radius, 2) * Math.PI;
-// document.write(`<p style="color: blue; font-size: xx-large;">Circumference = ${Circumference}</p>
-//     <p style="color: red; font-size: xx-large;">Circle Aria = ${CircleAria}</p>`);
-
-// -----------------------  + or -   -------------------------
-
-// let num1 = prompt("Enter the first number to check:");
-//   let num2 = prompt("Enter the second number to check:");
-// if (num1 == 0) {
-//   document.write(
-//     `<span style="color: black; font-size: xx-large;">Zero and </span>`
-//   );
-// } else {
-//   if (num1 > 0) {
-//     document.write(
-//       `<span style="color: blue; font-size: xx-large;">(${num1}) is Positive and </span>`
-//     );
-//   } else {
-//     document.write(
-//       `<span style="color: red; font-size: xx-large;">(${num1}) is Negative and </span>`
-//     );
-//   }
-// }
-// if (num2 == 0) {
-//   document.write(`<span style="color: black; font-size: xx-large;">Zero</p>`);
-// } else {
-//   if (num2 > 0) {
-//     document.write(
-//       `<span style="color: blue; font-size: xx-large;">(${num2}) is Positive</span>`
-//     );
-//   } else {
-//     document.write(
-//       `<span style="color: red; font-size: xx-large;">(${num2}) is Negative</span>`
-//     );
-//   }
-// }
-
+   
+    // let radius = Number(prompt("Enter a radius to :"));
+    // let Circumference = radius * 2 * Math.PI;
+    // let CircleAria = Math.pow(radius, 2) * Math.PI;
+    // document.write(`<p style="color: blue; font-size: xx-large;">Circumference = ${Circumference}</p>
+    //     <p style="color: red; font-size: xx-large;">Circle Aria = ${CircleAria}</p>`);
 // -------------------------------sum-------------------------
 
-let num1 = prompt("Enter the first number to sum :");
-let num2 = prompt("Enter the second number to sum:");
-document.write(
-    `<span style="color: red; font-size: xx-large;">sum = (${num1 + num2}) </span>`
-  );
+// let num1 = Number(prompt("Enter the first number1 to sum :"));
+// let num2 = Number(prompt("Enter the first number1 to sum :"));
+// document.write(
+//     `<span style="color: red; font-size: xx-large;">sum = (${num1 + num2}) </span>`
+// );
+  
+// --------------------------------------- + or -   ---------------------
+// let num = Number(prompt("Enter a number to check if it's + or -:"));
+// if (num == 0) {
+//         document.write(`<p style="color: blue; font-size: xx-large;">Zero</p>`);
+//     } else {
+//         if (num > 0) {
+//             document.write(
+//                 `<p style="color: blue; font-size: xx-large;">(${num}) is positive</p>`
+//             );
+//         } else {
+//             document.write(
+//                 `<p style="color: blue; font-size: xx-large;">(${num}) is nagative</p>`
+//             );
+//         }
+//     }
+// --------------------------reverse--------------------
+// let word = prompt("Enter a word to reverse it :");
+// document.write(`reverse of ${word} = `);
+// for (let i = (word.length-1); i >= 0; i--)
+// {
+//     document.write(word[i]);
+// }
+
